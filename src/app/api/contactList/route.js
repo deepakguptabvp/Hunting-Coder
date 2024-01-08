beb05e12504be0b06c8a8afef8cc40510bf41375
deepakguptabvp <deepakgupta.150743@gmail.com>
2024-01-08 23:47:18 +0530

    vercel commit final exp

--- src/app/api/contactList/route.js
+++ src/app/api/contactList/route.js
@@ -8,15 +8,15 @@ export async function POST(req, res) {
       try {
         console.log("123456789");
         let body = await req.json();
+
         let path = process.cwd() + "/src/app/api/contactList/contactdata";
         console.log("123456789", path);
+
         let data = await fs.promises.readdir(path);
         console.log("123456789", data);
-        // console.log(data, req.body, body);
-        responseData = await fs.promises.writeFile(
-          `${path}/${data.length + 1}.json`,
-          JSON.stringify(body)
-        );
+
+        
+        responseData = await fs.promises.writeFile(`${path}/${new Date().getTime()}.json`, JSON.stringify(body));
         console.log("123456789", responseData);
         return NextResponse.json(
           { message: "Your contact details", data: body, success: true },
