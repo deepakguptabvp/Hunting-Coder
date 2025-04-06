f5f103e71f6e9d184c72e3963fb9b640d190aa88
deepakguptabvp <deepakgupta.150743@gmail.com>
2025-04-06 15:22:19 +0530

    minor ui changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -65,7 +65,7 @@ const page = () => {
                 return (
                   <Link
                     href={`/blogpost/${blog.slug}`}
-                    className="group rounded-xl border border-gray-200 dark:border-gray-700 p-6 transition-all duration-300 hover:shadow-lg hover:border-purple-300 dark:hover:border-purple-700 bg-white dark:bg-gray-800 transform hover:-translate-y-1"
+                    className="group rounded-xl border border-gray-200 dark:border-gray-700 p-6 transition-all duration-300 hover:shadow-lg hover:border-purple-300 dark:hover:border-purple-700 bg-white dark:bg-gray-800 transform hover:-translate-y-3"
                     rel="noopener noreferrer"
                     key={index}
                   >
